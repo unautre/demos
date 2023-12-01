c53546878091b4c1b6c57866eff0fe8a57f5df95
Dylan Werner-Meier <dylan.bloger@gmail.com>
2023-12-01 15:31:51 +0100

    WIP

--- test-netty/src/main/java/dev/bandarlog/test/netty/proxy/http/ProxyLogicHandler.java
+++ test-netty/src/main/java/dev/bandarlog/test/netty/proxy/http/ProxyLogicHandler.java
@@ -1,13 +1,19 @@
 package dev.bandarlog.test.netty.proxy.http;
 
+import java.nio.charset.StandardCharsets;
+
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.QueryStringDecoder;
 
 public class ProxyLogicHandler extends ChannelDuplexHandler {
-
+	
+	private boolean dump = false;
+	
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		
@@ -15,6 +21,13 @@ public class ProxyLogicHandler extends ChannelDuplexHandler {
 			final HttpRequest req = (HttpRequest) msg;
 			
 			System.out.printf(">>> %s %s %s%n", req.method(), req.uri(), req.protocolVersion());
+			System.out.println(">>> " + req.headers());
+			
+			final QueryStringDecoder queryDecoder = new QueryStringDecoder(req.uri());
+			
+			dump = (queryDecoder.parameters().get("partNumber") == null);
+		} else if (msg instanceof HttpContent && dump) {
+			System.out.println("> " + msg);
 		}
 		
 		super.channelRead(ctx, msg);
@@ -26,7 +39,11 @@ public class ProxyLogicHandler extends ChannelDuplexHandler {
 		if (msg instanceof HttpResponse) {
 			final HttpResponse resp = (HttpResponse) msg;
 			
-			System.out.printf("<<< %d %s%n", resp.status().code(), resp.status().codeAsText());
+			System.out.printf("<<< %d %s%n", resp.status().code(), resp.status().reasonPhrase());
+		} else if (msg instanceof HttpContent) {
+			final HttpContent content = (HttpContent) msg;
+			
+			System.out.println("< " + content.content().toString(StandardCharsets.UTF_8).replaceAll("\n(.)", "\n< $1"));
 		}
 		
 		super.write(ctx, msg, promise);
